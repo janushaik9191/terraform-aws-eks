ab0facd5b96334af4cf3f05af9210deda7e250ea
meerjan <janushaik9191@gmail.com>
2024-11-13 23:28:11 +0530

    terraform-aws-eks

---
+++ 10-vpc/provider.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.75.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "meerjan-s3-remote"
+    key    = "expense-vpc"
+    region = "us-east-1"
+    dynamodb_table = "meerjan-locking"
+  }
+}
+
+provider "aws" {
+  # Configuration options
+  region = "us-east-1"
+}
